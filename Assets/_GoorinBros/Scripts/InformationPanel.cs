35d2f7c993cc969b3105bc3ba0b34fe7eb072a52
EmersonJara <Jaragamarra28>
2019-08-14 16:58:55 -0500

    Reset del Scroll en el panel de informacion.

--- Assets/_GoorinBros/Scripts/InformationPanel.cs
+++ Assets/_GoorinBros/Scripts/InformationPanel.cs
@@ -41,7 +41,7 @@ namespace goorinAR
         [SerializeField]
         private GameObject empty;
         [SerializeField]
-        private List<GameObject> infoBasic;
+        private ScrollRect scrollInfomation;
         [SerializeField]
         private Transform info;
         [SerializeField]
@@ -198,16 +198,14 @@ namespace goorinAR
 
         private void ResetInfoAnimation()
         {
+
+            scrollInfomation.verticalNormalizedPosition = 1;
+
             if (isEnabled)
             {
                 info.DOLocalMoveY(0, 0.2f);
                 portada.DOScale(new Vector3(1.5f, 1.5f, 1.5f), 0.2f);
                 empty.SetActive(true);
-
-                //for (int i = 0; i < infoBasic.Count; i++)
-                //{
-                //    infoBasic[i].SetActive(false);
-                //}
                 isEnabled = false;
             }
         }
@@ -219,10 +217,7 @@ namespace goorinAR
                 info.DOLocalMoveY(280, 0.2f);
                 portada.DOScale(Vector3.one, 0.2f);
                 empty.SetActive(false);
-                //for (int i = 0; i < infoBasic.Count; i++)
-                //{
-                //    infoBasic[i].SetActive(true);
-                //}
+              
                 isEnabled = true;
             }
             else
@@ -230,10 +225,7 @@ namespace goorinAR
                 info.DOLocalMoveY(0, 0.2f);
                 portada.DOScale(new Vector3(1.5f, 1.5f, 1.5f), 0.2f);
                 empty.SetActive(true);
-                //for (int i = 0; i < infoBasic.Count; i++)
-                //{
-                //    infoBasic[i].SetActive(false);
-                //}
+                
                 isEnabled = false;
             }
         }
